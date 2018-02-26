55dda1a0e870410c934c1fbc0dde2da82cd05ff3
Radonirina Ranaivonjanahary <radonirina.ranaivonjanahary@netapsys.fr>
2018-02-26 17:28:11 +0300

    add chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonPipeModule } from './pipes/common-pipe.module';
-
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CommonPipeModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/detail/detail.module.ts
+++ src/app/components/detail/detail.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { DetailRoutingModule } from './detail-routing.module';
 import { SurveyDetailComponent } from './survey-detail/survey-detail.component';
+import { ChartDirective } from '../../directives/chart.directive';
+
 
 @NgModule({
   imports: [
     CommonModule,
     DetailRoutingModule
   ],
-  declarations: [SurveyDetailComponent]
+  declarations: [SurveyDetailComponent, ChartDirective]
 })
 export class DetailModule { }
